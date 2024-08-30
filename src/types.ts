bda66967092734fe3f7b7c29051ceebe81dbd0e4
Jacob Cozens <jacob.cozens@matchesfashion.com>
2024-08-30 14:39:44 +0100

    include type definitions

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type UfoSighting = {
+  datetime: string;
+  city: string;
+  state: string;
+  country: string;
+  shape: string;
+  "duration (seconds)": string;
+  "duration (hours/min)": string;
+  comments: string;
+  "date posted": string;
+  latitude: string;
+  longitude: string;
+  __parsed_extra?: any[];
+};
