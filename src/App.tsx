7951bfe47e7380e4883ef9086f03e19fb6abc814
Jacob Cozens <jacob.cozens@matchesfashion.com>
2024-09-01 21:30:45 +0100

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { UfoDataProvider } from "./context/ufoData";
+import styles from "./App.module.scss";
+import ChartGrid from "./components/chart-grid/ChartGrid";
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UfoDataProvider>
+      <div className={styles.container}>
+        <div className={styles.main}>
+          <Header />
+          <ChartGrid />
+        </div>
+      </div>
+    </UfoDataProvider>
   );
 }
 
